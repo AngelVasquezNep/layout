2ed88beff9daa1012e1401b1f5a13f5475f76af5
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-04-05 03:55:17 -0500

    Trabajando con eventBus y barra de tamaño

---
+++ components/Barra.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  .barra.center
+    p Hazme pequeño para que veas la mágia. Estoy al 
+      strong {{ valor }} %
+    input.input(type="range", v-model="valor", min="45") 
+</template>
+
+<script>
+  export default {
+    name: 'Barra',
+    data () {
+      return {
+        valor: 100
+      }
+    },
+
+    watch: {
+      valor (newValor, oldValor) {
+        this.$bus.$emit('valor', newValor)
+        console.log(newValor)
+      }
+    }
+  }
+</script>
+
+<style lang="sass" scoped>
+
+.barra
+  font-family: 'Courier New', Courier, monospace;
+  width: 100%
+  color: #1f1f1f
+  background-color: #00C485
+  justify-content: initial
+  .input
+    margin: 0px auto
+    margin-bottom: 10px
+    width: 80%
+    
+</style>
--- components/Header.vue
+++ components/Header.vue
@@ -1,20 +1,47 @@
 <template lang="pug">
-  .header.center
-    h1.title CSS GRID LAYOUT
-    router-link.home(to="/") Home
-    router-link.instagram(to="instagram") Instagram
-    router-link.platzi(to="platzi") Platzi
-    router-link.pinterest(to="pinterest") Pinterest
-    
+  .header-container
+    .header.center(:class="valor1")
+      h1.title CSS GRID LAYOUT
+      router-link.home(to="/") Home
+      router-link.instagram(to="instagram") Instagram
+      router-link.platzi(to="platzi") Platzi
+      router-link.pinterest(to="pinterest") Pinterest
+    Barra
+
 </template>
 
 <script>
-  export default {
-    name: "Header"
+import Barra from '~/components/Barra.vue'
+
+export default {
+  name: "Header",
+  components: { Barra },
+  data () {
+    return {
+      valor: 100
+    }
+  },
+
+  mounted() {
+    this.$bus.$on('valor', (newValor) => {
+      this.valor = newValor
+    })
+  },
+
+  computed: {
+    valor1 () {
+      return {
+        is768: this.valor < 90
+      }
+    }
   }
+}
 </script>
 
 <style lang="sass" scoped>
+  .red
+    border: 1px solid red
+
   .header
     padding: 10px 0
     color: #fff
@@ -22,8 +49,9 @@
     grid-template-areas: "title title home instagram platzi pinterest"
     background-color: #32aaaa
     background-color: #3B8070
-    margin-bottom: 20px
     height: 70px 
+    transition: .5s all
+    // margin-bottom: 20px
     
     .title
       grid-area: title
@@ -47,5 +75,9 @@
       grid-template-columns: repeat(4, 1fr)
       grid-template-areas: "title title title title" "home instagram platzi pinterest"
 
-
+  .is768
+    width: 768px
+    height: 100px
+    grid-template-columns: repeat(4, 1fr)
+    grid-template-areas: "title title title title" "home instagram platzi pinterest"
 </style>
